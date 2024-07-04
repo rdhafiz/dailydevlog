f22811a7c47959b25b45a5233be23356f3e025d8
Ali Haider Swccho <swccho263@gmail.com>
2024-07-04 17:14:20 +0600

    [master]

--- resources/views/user-panel/layout/layout.blade.php
+++ resources/views/user-panel/layout/layout.blade.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html lang="en">
+    <!-- All meta, links, scripts title or any other tags we need in head will be added in the head component -->
     @include('user-panel.layout.includes.head')
+    
     <body class="bg-white dark:bg-gray-900 text-black dark:text-white h-lvh grid place-items-center justify-center ">
     <div class="text-center">
         <button onclick="toggleTheme()" class="text-2xl border p-5 mb-4 rounded-2xl">Change Theme</button>

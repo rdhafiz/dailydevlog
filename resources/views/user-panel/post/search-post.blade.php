9b8a7085ab67137cfee657401996a92cbd8aaed2
mahi-bashar-akash <mahibashar2023@gmail.com>
2024-09-04 19:10:09 +0600

    '[update-tag]'

--- resources/views/user-panel/post/search-post.blade.php
+++ resources/views/user-panel/post/search-post.blade.php
@@ -4,7 +4,9 @@
 
     <div id="searchBlogs" class="fixed-container mt-[50px]">
 
-        <div class="mb-5 text-secondary dark:text-white block font-bold text-[16px] leading-[24px] dark:hover:text-second hover:text-second duration-500 text-truncate-line-2">Showing blogs matching the search for <span class="text-second">@{{ formData.keyword }}</span></div>
+        <div
+            class="mb-5 text-secondary dark:text-white block font-bold text-[16px] leading-[24px] dark:hover:text-second hover:text-second duration-500 text-truncate-line-2">
+            Showing blogs matching the search for <span class="text-second">@{{ formData.keyword }}</span></div>
 
         <div class="w-full px-4" v-if="tableData.length === 0 && !loading">
             <div
@@ -30,27 +32,41 @@
         <template v-if="tableData.length > 0 && !loading">
             <div class="grid grid-cols-1 sm:grid-cols-2 min-[917px]:grid-cols-3 gap-[15px]">
                 <template v-for="(each, index) in tableData">
-                    <div class="group rounded-2xl bg-white dark:bg-[#222222] shadow-lg sm:h-[316px]" :class="{ 'sm:col-span-2 block sm:grid sm:grid-cols-2 sm:gap-[15px]' : index === 0, 'col-span-1 lg:col-span-1 flex flex-col' : index > 0 }">
-                        <a :href="`/blog-details/`+each.id" class="block rounded-2xl overflow-hidden min-h-[120px]" :class="{ 'sm:w-[316px] sm:h-[316px] sm:col-span-1 h-[120px] w-full' : index === 0, 'h-[125px]' : index > 0 }">
+                    <div class="group rounded-2xl bg-white dark:bg-[#222222] shadow-lg sm:h-[316px]"
+                         :class="{ 'sm:col-span-2 block sm:grid sm:grid-cols-2 sm:gap-[15px]' : index === 0, 'col-span-1 lg:col-span-1 flex flex-col' : index > 0 }">
+                        <a :href="`/blog-details/`+each.id" class="block rounded-2xl overflow-hidden min-h-[120px]"
+                           :class="{ 'sm:w-[316px] sm:h-[316px] sm:col-span-1 h-[120px] w-full' : index === 0, 'h-[125px]' : index > 0 }">
                             <img :src="'/storage/media/'+ each.featured_image"
                                  class="!h-full w-full rounded-2xl object-cover scale-[1] group-hover:scale-[1.2] duration-500"
                                  alt="blog image" v-if="each.featured_image">
-                            <img src="{{asset('/images/default.png')}}" class="h-full w-full rounded-2xl object-cover scale-[1] group-hover:scale-[1.2] duration-500"
+                            <img src="{{asset('/images/default.png')}}"
+                                 class="h-full w-full rounded-2xl object-cover scale-[1] group-hover:scale-[1.2] duration-500"
                                  alt="blog image" v-if="!each.featured_image">
                         </a>
-                        <div class="grow flex flex-col gap-0 justify-between overflow-auto" :class="{ 'sm:col-span-1 text-center sm:text-left pr-[6px] pt-[15px] pb-2' : index === 0, 'px-2 pt-2 pb-1 text-center' : index > 0 }">
+                        <div class="grow flex flex-col gap-0 justify-between overflow-auto"
+                             :class="{ 'sm:col-span-1 text-center sm:text-left pr-[6px] pt-[15px] pb-2' : index === 0, 'px-2 pt-2 pb-1 text-center' : index > 0 }">
                             <div class="overflow-auto">
-                                <a :href="`/blog-details/`+each.id" class="text-secondary dark:text-white block font-bold dark:hover:text-second hover:text-second duration-500" :class="{ 'text-truncate-line-3 text-[20px] leading-[30px]' : index === 0, 'text-truncate-line-2 text-[17px] leading-[25.5px]' : index > 0 }">
+                                <a :href="`/blog-details/`+each.id"
+                                   class="text-secondary dark:text-white block font-bold dark:hover:text-second hover:text-second duration-500"
+                                   :class="{ 'text-truncate-line-3 text-[20px] leading-[30px]' : index === 0, 'text-truncate-line-2 text-[17px] leading-[25.5px]' : index > 0 }">
                                     @{{ each.title }}
                                 </a>
-                                <div class="flex flex-wrap items-center pt-1 pb-[12px]" :class="{ 'gap-x-[9px] justify-center sm:justify-start' : index === 0, 'gap-x-[8px] justify-center' : index > 0 }">
-                                    <div v-for="(tag, indexTag) in each?.tags" class="rounded-2xl leading-[14px] font-[400] text-center flex justify-center items-center capitalize" :class="{'bg-primary !text-white': indexTag === 0, 'bg-first !text-[#333333]': indexTag === 1, 'bg-dark3 !text-white': indexTag === 2, 'bg-red !text-white': indexTag === 3, 'w-[65px] h-[20px] text-[12px]' : index === 0, 'w-[50px] h-[16px] text-[11px]' : index > 0 }">@{{ tag }}</div>
+                                <div class="flex flex-wrap items-center pt-1 pb-[12px]"
+                                     :class="{ 'gap-x-[9px] justify-center sm:justify-start' : index === 0, 'gap-x-[8px] justify-center' : index > 0 }">
+                                    <div v-for="(tag, indexTag) in each?.tags"
+                                         class="rounded-2xl leading-[14px] font-[400] text-center flex justify-center items-center capitalize"
+                                         :class="{'bg-primary !text-white': indexTag === 0, 'bg-first !text-[#333333]': indexTag === 1, 'bg-dark3 !text-white': indexTag === 2, 'bg-red !text-white': indexTag === 3, 'w-[65px] h-[20px] text-[12px]' : index === 0, 'w-[50px] h-[16px] text-[11px]' : index > 0 }">
+                                        @{{ tag }}
+                                    </div>
                                 </div>
-                                <div class="font-[400] leading-[21px] text-black dark:!text-light2" :class="{ 'text-[14px] text-truncate-line-5' : index === 0, 'text-[13px] text-truncate-line-3' : index > 0 }">
+                                <div class="font-[400] leading-[21px] text-black dark:!text-light2"
+                                     :class="{ 'text-[14px] text-truncate-line-5' : index === 0, 'text-[13px] text-truncate-line-3' : index > 0 }">
                                     <p>@{{ each.short_description }}</p>
                                 </div>
                             </div>
-                            <div class="flex items-center font-[400] text-secondary dark:!text-light2 text-[13px] leading-[19.5px] pb-[2px] gap-1" :class="{ 'justify-center sm:justify-start' : index === 0, 'justify-center' : index > 0 }">
+                            <div
+                                class="flex items-center font-[400] text-secondary dark:!text-light2 text-[13px] leading-[19.5px] pb-[2px] gap-1"
+                                :class="{ 'justify-center sm:justify-start' : index === 0, 'justify-center' : index > 0 }">
                                 <span>@{{ each.published_at_format }}</span>
                                 <span>•</span>
                                 <span>3m Read</span>

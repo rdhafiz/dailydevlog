48cccc86f9440e338b1bb031b190b8f4ad34eb67
mahi-bashar-akash <mahibashar2023@gmail.com>
2024-09-11 12:56:02 +0600

    'modify-layout'

--- resources/views/user-panel/layout/includes/head.blade.php
+++ resources/views/user-panel/layout/includes/head.blade.php
@@ -1,7 +1,7 @@
 <head>
+    
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> @yield('title') </title>
     <meta name="keywords" content="Daily Dev Log, programming blog, IT tutorials, technology articles, web development, mobile app development, coding tips, software development, tech news, programming insights, developer resources"/>

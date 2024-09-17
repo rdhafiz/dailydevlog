ca0e69e414858b8e5d13721fe3567a2e4faed6f8
Noyon Ahammed Sojon <ahammedsojon000@gmail.com>
2024-09-17 16:19:48 +0600

    custom gradiant added in profile page

--- resources/views/user-panel/profile/profile.blade.php
+++ resources/views/user-panel/profile/profile.blade.php
@@ -110,10 +110,10 @@
                             {{-- Submit Button --}}
                             <div class="flex justify-start items-center">
                                 <span id="profileUpdateBtn">
-                                    <button type="submit" class="btn h-[45px] flex justify-center text-white items-center border border-[#0000003F] bg-gradient-to-r from-15% from-[#85A41C] via-50% via-[#AED725] to-85% to-[#85A41C] rounded-md w-[120px]"> Submit </button>
+                                    <button type="submit" class="btn h-[45px] flex justify-center text-black items-center border border-[#0000003F] custom-gradient rounded-md w-[120px]"> Submit </button>
                                 </span>
                                 <span id="profileUpdateLoading" class="hidden">
-                                    <button type="button" class="btn h-[45px] flex justify-center text-white items-center border border-[#0000003F] bg-gradient-to-r from-15% from-[#85A41C] via-50% via-[#AED725] to-85% to-[#85A41C] rounded-md w-[120px]" disabled>
+                                    <button type="button" class="btn h-[45px] flex justify-center text-black items-center border border-[#0000003F] custom-gradient rounded-md w-[120px]" disabled>
                                         <svg class="h-5 mx-auto w-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
@@ -160,10 +160,10 @@
                             </div>
                             <div class="flex justify-start items-center">
                                 <span id="changePasswordBtn">
-                                    <button type="submit" class="btn h-[45px] flex justify-center text-white items-center border border-[#0000003F] bg-gradient-to-r from-15% from-[#85A41C] via-50% via-[#AED725] to-85% to-[#85A41C] rounded-md w-[120px]"> Submit </button>
+                                    <button type="submit" class="btn h-[45px] flex justify-center text-black items-center border border-[#0000003F] custom-gradient rounded-md w-[120px]"> Submit </button>
                                 </span>
                                 <span id="changePasswordLoader" class="hidden">
-                                    <button type="button" class="btn h-[45px] flex justify-center text-white items-center border border-[#0000003F] bg-gradient-to-r from-15% from-[#85A41C] via-50% via-[#AED725] to-85% to-[#85A41C] rounded-md w-[120px]" disabled>
+                                    <button type="button" class="btn h-[45px] flex justify-center text-black items-center border border-[#0000003F] custom-gradient rounded-md w-[120px]" disabled>
                                         <svg class="h-5 mx-auto w-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
